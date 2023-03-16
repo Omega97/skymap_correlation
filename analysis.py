866311a919a7d7080d8474c65d612cdadbe67228
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2023-03-16 16:31:06 +0100

    overall clean-up

--- analysis.py
+++ analysis.py
@@ -1,71 +1,74 @@
 """Sky-map"""
 import healpy as hp
 from astropy.io import fits
-import numpy as np
 from typing import List
-
-try:
-    from misc import histogram_equalized
-    from utils import correlation, proximity_masks, Mask
-except ImportError:
-    from .misc import histogram_equalized
-    from .utils import correlation, proximity_masks, Mask
+from misc import histogram_equalized, take_time
+from utils import correlation, compute_proximity_masks, Mask, correlation_map
 
 
-class SkyMapAnalysis:
+class SkyMapAnalyzer:
     """
     This class performs the analysis of the correlation between sky-maps
     """
     def __init__(self, *map_paths, verbose=False):
         self.paths = map_paths
+        self.verbose = verbose
         self.maps = None
-        self.normalized_maps = None
-        self.nside = None
+        self.n_side = None
         self.n_pixels = None
-
-        self._load_data(verbose=verbose)
-        self._normalize_maps()
+        self._load_data()
 
     def __len__(self):
         return len(self.paths)
 
-    def _load_data(self, verbose=False):
+    def __iter__(self):
+        return iter(self.maps)
+
+    @take_time
+    def _load_data(self):
         """load locally-saved data"""
         self.maps = []
         for i in range(len(self)):
             with fits.open(self.paths[i]) as lambda_list_1:
-                if verbose:
+                if self.verbose:
+                    print()
                     lambda_list_1.info()
                 self.maps.append(hp.read_map(lambda_list_1))
 
-    def _normalize_maps(self):
-        self.normalized_maps = []
+    @take_time
+    def normalize(self):
+        normalized_maps = []
+        for sky_map in self:
+            normalized_maps.append(histogram_equalized(sky_map))
+        self.maps = normalized_maps
+
+    @take_time
+    def downscale(self, new_n_side):
+        self.n_side = new_n_side
+        self.n_pixels = hp.nside2npix(new_n_side)
         for i in range(len(self)):
-            self.normalized_maps.append(histogram_equalized(self.maps[i]))
+            self.maps[i] = hp.ud_grade(map_in=self.maps[i], nside_out=new_n_side)
 
-    def downscale(self, nside):
-        self.nside = nside
-        self.n_pixels = hp.nside2npix(nside)
-        for i in range(len(self)):
-            self.maps[i] = hp.ud_grade(map_in=self.maps[i], nside_out=nside)
-        self._normalize_maps()
+    @take_time
+    def compute_global_correlation(self, i=0, j=1):
+        """ Global correlation of the whole normalized maps of indices i and j """
+        return correlation(self.maps[i], self.maps[j])
 
-    def compute_global_correlation(self):
-        """global correlation of the whole normalized maps"""
-        return correlation(self.normalized_maps[0], self.normalized_maps[1])
-
-    def compute_custom_correlation(self, proximity_masks: List[Mask]):
+    @take_time
+    def compute_custom_correlation_maps(self, proximity_matrix: List[Mask], i=0, j=1):
         """
-        compute the correlation between the maps for each pixel given a list of proximity masks
-        :param proximity_masks: list of masks that define whether two pixels are close or not
+        Given a list of proximity masks, compute the correlation
+        between masked maps of indices i and j
+
+        :param proximity_matrix: list of masks that define whether two pixels are close or not
+        :param i: first map index
+        :param j: second map index
         :return: sky-map of correlation values
         """
-        v = [correlation(self.normalized_maps[0][mask.indices],
-                         self.normalized_maps[1][mask.indices])
-             for mask in proximity_masks]
-        return np.array(v)
+        return correlation_map(self.maps[i], self.maps[j], proximity_matrix)
 
-    def compute_local_correlation(self, angle_rad=.2):
+    @take_time
+    def compute_local_correlation(self, angle_rad=.2, i=0, j=1):
         """for each pixel, compute the correlation between the maps for pixels within a certain angle"""
-        masks = proximity_masks(self.nside, angle_rad)
-        return self.compute_custom_correlation(masks)
+        masks = compute_proximity_masks(self.n_side, angle_rad)
+        return self.compute_custom_correlation_maps(masks, i=i, j=j)
